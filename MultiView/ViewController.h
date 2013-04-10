cff0a71097ed35a288e653facdc2f7d9d2a05f01
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-04-10 09:18:59 +0800

    Initial Commit

---
+++ MultiView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultiView
+//
+//  Created by Peter Pan on 4/10/13.
+//  Copyright (c) 2013 Peter Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MultiViewTests/MultiViewTests.m
@@ -0,0 +1,32 @@
+//
+//  MultiViewTests.m
+//  MultiViewTests
+//
+//  Created by Peter Pan on 4/10/13.
+//  Copyright (c) 2013 Peter Pan. All rights reserved.
+//
+
+#import "MultiViewTests.h"
+
+@implementation MultiViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MultiViewTests");
+}
+
+@end
